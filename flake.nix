6f1f7b87c7457405711bef88364560f441ff8126
Akira Komamura <akira.komamura@gmail.com>
2021-12-27 15:12:39 +0900

    chore: Drop the pre-commit configuration from the flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,7 @@
 {
-  description = "Personal collection of flake templates";
+  description = "A collection of project templates";
 
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
-
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , pre-commit-hooks
-    }:
+  outputs = { ... }:
     {
       templates = {
         minimal = {
@@ -32,24 +17,5 @@
           description = "Elixir Phoenix project where you use Mix";
         };
       };
-    } //
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              nix-linter.enable = true;
-            };
-          };
-        };
-        devShell = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-        };
-      }));
+    };
 }
