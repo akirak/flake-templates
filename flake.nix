f7daf7ae77c8e14d0204aff91cc282db3229b415
Akira Komamura <akira.komamura@gmail.com>
2022-04-08 19:53:05 +0900

    chore: Add pre-commit hook (with alejandra formatter)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   description = "A collection of project templates";
 
-  outputs = { ... }:
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
     {
       templates = {
         meta = {
@@ -21,5 +30,23 @@
           description = "Elixir Phoenix project where you use Mix";
         };
       };
-    };
+    }
+    // flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
+          };
+        };
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      }
+    );
 }
