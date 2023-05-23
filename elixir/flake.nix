3c658b98a85dd6a048c4168358aee2fbe1f57617
akirak <akirak@users.noreply.github.com>
2023-05-23 04:02:29 +0000

    Update

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -12,7 +12,7 @@
         };
 
         # Set the Erlang version
-        erlangVersion = "erlangR25";
+        erlangVersion = "erlangR26";
         # Set the Elixir version
         elixirVersion = "elixir_1_14";
 
