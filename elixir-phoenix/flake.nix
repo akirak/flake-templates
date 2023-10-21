fcbb897410d0e6a2c0dafbb3a3bb310d659adff3
Akira Komamura <akira.komamura@gmail.com>
2023-10-21 20:39:25 +0900

    elixir-phoenix: Remove flake-utils dependency

--- elixir-phoenix/flake.nix
+++ elixir-phoenix/flake.nix
@@ -1,71 +1,80 @@
 {
-  inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
+  inputs = {
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
   };
-  inputs.systems.url = "github:nix-systems/default";
 
   outputs = {
     self,
     nixpkgs,
     systems,
-    flake-utils,
     pre-commit-hooks,
-  }:
-    flake-utils.lib.eachSystem (import systems)
-    (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+
+    # Set the Erlang version
+    erlangVersion = "erlang_25";
+    # Set the Elixir version
+    elixirVersion = "elixir_1_15";
+
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f (import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: _: let
+                erlang = final.beam.interpreters.${erlangVersion};
+                beamPackages = final.beam.packages.${erlangVersion};
+                elixir = beamPackages.${elixirVersion};
+              in {
+                inherit erlang elixir;
+                inherit (beamPackages) elixir-ls hex;
+              })
+            ];
+          })
+      );
+  in {
+    # TODO: Add your Elixir package
+    # packages = flake-utils.lib.flattenTree {
+    # } ;
+
+    checks = eachSystem (pkgs: {
+      pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
+        src = ./.;
+        hooks = {
+          # TODO: Add a linter for Elixir
         };
+      };
+    });
 
-        # Set the Erlang version
-        erlangVersion = "erlangR25";
-        # Set the Elixir version
-        elixirVersion = "elixir_1_15";
-
-        erlang = pkgs.beam.interpreters.${erlangVersion};
-        beamPackages = pkgs.beam.packages.${erlangVersion};
-        elixir = beamPackages.${elixirVersion};
-
-        inherit (pkgs.lib) optional optionals;
-
-        fileWatchers = with pkgs; (optional stdenv.isLinux inotify-tools
-        ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-          CoreFoundation
-          CoreServices
-        ]));
-      in rec {
-        # TODO: Add your Elixir package
-        # packages = flake-utils.lib.flattenTree {
-        # } ;
-
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              # TODO: Add a linter for Elixir
-            };
-          };
-        };
-        devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
-          buildInputs =
-            [
-              erlang
-              elixir
-              beamPackages.elixir-ls
-            ]
-            ++ (with pkgs; [
-              nodejs
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; (
+          [
+            erlang
+            elixir
+            elixir-ls
+            nodejs
+          ]
+          ++ lib.optional stdenv.isLinux inotify-tools
+          ++ (
+            lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+              CoreFoundation
+              CoreServices
             ])
-            ++ fileWatchers;
+          )
+        );
 
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
 
-          LANG = "C.UTF-8";
-          ERL_AFLAGS = "-kernel shell_history enabled";
-        };
-      }
-    );
+        LANG = "C.UTF-8";
+        ERL_AFLAGS = "-kernel shell_history enabled";
+      };
+    });
+  };
 }
