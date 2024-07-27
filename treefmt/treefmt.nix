85ba8692db6db46d4841e0edc3646b2f10cfdbc5
Akira Komamura <akira.komamura@gmail.com>
2024-07-27 18:32:59 +0900

    feat(treefmt): Encourage use of nixfmt-rfc-style

--- treefmt/treefmt.nix
+++ treefmt/treefmt.nix
@@ -2,7 +2,7 @@
   projectRootFile = "treefmt.nix";
 
   # Format Nix
-  programs.alejandra.enable = true;
+  programs.nixfmt-rfc-style.enable = true;
 
   # You can add formatters for your languages.
   # See https://github.com/numtide/treefmt-nix#supported-programs
