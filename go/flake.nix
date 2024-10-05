172aed43dc6731c56427a2a5dd135428cd0d2659
Akira Komamura <akira.komamura@gmail.com>
2024-10-05 14:02:53 +0900

    feat: Add go

---
+++ go/flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # Configure a binary cache for your executable(s).
+  nixConfig = {
+    extra-substituters =
+      [
+      ];
+    extra-trusted-public-keys =
+      [
+      ];
+  };
+
+  outputs =
+    {
+      self,
+      systems,
+      nixpkgs,
+      treefmt-nix,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = f: lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+    in
+    {
+      # Build executables. See https://nixos.org/manual/nixpkgs/stable/#sec-language-go
+      packages = eachSystem (pkgs: {
+        # default = pkgs.buildGoModule {
+        #   pname = "squasher";
+        #   version = builtins.substring 0 8 (self.lastModifiedDate or "19700101");
+        #   src = self.outPath;
+        #   vendorHash = lib.fakeHash;
+        #   meta = { };
+        # };
+      });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+          ];
+        };
+      });
+
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+      });
+    };
+}
