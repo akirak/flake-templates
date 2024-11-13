7897cbce76e5601009231146784d21da80b4dde7
Akira Komamura <akira.komamura@gmail.com>
2024-11-13 18:47:26 +0900

    feat(flake-parts): Init

---
+++ flake-parts/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    # treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        # inputs.flake-parts.flakeModules.partitions
+        # inputs.treefmt-nix.flakeModule
+      ];
+
+      # partitions.dev = {
+      #   extraInputsFlake = ./dev;
+      #   module = {
+      #     imports = [ ./dev/flake-module.nix ];
+      #   };
+      # };
+
+      # partitionedAttrs = {
+      #   checks = "dev";
+      #   devShells = "dev";
+      # };
+
+      perSystem =
+        {
+          system,
+          pkgs,
+          inputs',
+          ...
+        }:
+        {
+          # You can use `extend' to extend the packages with an overlay (or use
+          # `import inputs.nixpkgs { ... }`).
+          _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
+        };
+    };
+}
