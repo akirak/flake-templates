5a59ba0c78fb29fa6fa45648faa346b62cc5f09b
Akira Komamura <akira.komamura@gmail.com>
2025-01-19 11:04:40 +0900

    docs: Add MIT license identifiers

--- doc/src/content/config.ts
+++ doc/src/content/config.ts
@@ -1,3 +1,8 @@
+/*
+  SPDX-FileCopyrightText: 2024-2025 Akira Komamura
+
+  SPDX-License-Identifier: MIT
+*/
 import { defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
