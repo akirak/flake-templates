9a003329ebb373aaffc242473980514fbfd57ebb
Akira Komamura <akira.komamura@gmail.com>
2025-08-16 03:46:49 +0900

    fix(flake-parts): Fix the pkgs arg

--- flake-parts/flake.nix
+++ flake-parts/flake.nix
@@ -39,7 +39,7 @@
         {
           # You can use `extend' to extend the packages with an overlay (or use
           # `import inputs.nixpkgs { ... }`).
-          _module.args.pkgs = inputs'.nixpkgs.legacyPackages.${system};
+          _module.args.pkgs = inputs.nixpkgs.legacyPackages.${system};
 
           devShells.default = pkgs.mkShell {
             packages = [ ];
