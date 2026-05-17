534714205aa87f7c72e4d2053e88e32159242e5b
Akira Komamura <akira.komamura@gmail.com>
2026-05-17 21:22:14 +0900

    style(doc): Reformat Nix

--- doc/flake.nix
+++ doc/flake.nix
@@ -18,25 +18,30 @@
       ...
     }:
     let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f system nixpkgs.legacyPackages.${system});
+      eachSystem =
+        f: nixpkgs.lib.genAttrs (import systems) (system: f system nixpkgs.legacyPackages.${system});
 
       treefmtEval = eachSystem (_system: pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in
     {
-      devShells = eachSystem (_system: pkgs: {
-        default = pkgs.mkShell {
-          packages = [
-            pkgs.nodejs
-            pkgs.corepack
-            pkgs.typescript-go
-          ];
-        };
-      });
+      devShells = eachSystem (
+        _system: pkgs: {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs
+              pkgs.corepack
+              pkgs.typescript-go
+            ];
+          };
+        }
+      );
 
       formatter = eachSystem (system: _pkgs: treefmtEval.${system}.config.build.wrapper);
 
-      checks = eachSystem (system: _pkgs: {
-        treefmt = treefmtEval.${system}.config.build.check self;
-      });
+      checks = eachSystem (
+        system: _pkgs: {
+          treefmt = treefmtEval.${system}.config.build.check self;
+        }
+      );
     };
 }
